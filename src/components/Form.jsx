3049d110ed85345c0348e4850f48ee1d0829b066
CanbulatOnarok <canbulatonarok@gmail.com>
2023-10-25 15:51:45 +0300

    team list css düzenleme

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,21 +5,7 @@ const Form = () => {
 
     <div className='form'>
       <h1>Create New F1 Team </h1>
-      <select name="" id="">
-        <option value="">Team Name</option>
-        <option value="">Red Bull</option>
-        <option value="">Mercedes</option>
-        <option value="">Scuderia Ferrari</option>
-        <option value="">McLaren</option>
-        <option value="">Aston Martin"</option>
-        <option value="">Alpine</option>
-        <option value="">Williams</option>
-        <option value="">Alfa Romeo</option>
-        <option value="">Haas</option>
-        <option value="">Alpha Tauri</option>
-
-
-      </select>
+      <input type="text" placeholder='Team Name:' />
       <input type="text" placeholder='Drivers:' />
       <input type="text" placeholder='Base:' />
       <input type="text" placeholder='Team Chief:' />
