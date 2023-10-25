249ba7c55e91b5d6c1baf797e85e540e435c29cd
CanbulatOnarok <canbulatonarok@gmail.com>
2023-10-25 18:12:18 +0300

    güncellemeler

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ function App() {
 
   const [f1, setF1] = useState([]);
 
+  // Dataları getirme...
   const getData = async () => {
     const response =await axios.get("http://localhost:3005/f1");
     const f1 =await response.data;
     setF1(f1);
     console.log(f1);
   }
+
+// Yeni data ekleme
+const cretaTeam = (newTeam) =>{
+  setF1(prev=>[...prev,newTeam]);
+}
+
 
 useEffect(()=>{
   getData();
@@ -28,7 +35,7 @@ useEffect(()=>{
   return (
     <>
       <Navbar />
-      <Form />
+      <Form cretaTeam={cretaTeam}/>
       <TeamList f1={f1}/>
     </>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import "../style/Form.scss"
-const Form = () => {
+import { useState } from 'react'
+const Form = ({cretaTeam}) => {
+  const [teamName, setTeamName] = useState("");
+  const [drivers, setDrivers] = useState("");
+  const [base, setBase] = useState("");
+  const [teamChief, setTeamChief] = useState("");
+  const [chassis, setChassis] = useState("");
+  const [logo, setLogo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    cretaTeam({
+      teamName: teamName,
+      drivers: drivers,
+      base: base,
+      teamChief: teamChief,
+      chassis: chassis,
+      logo: logo
+    })
+    setTeamName("");
+    setDrivers("");
+    setBase("");
+    setTeamChief("");
+    setChassis("");
+    setLogo("");
+  }
+
   return (
 
     <div className='form'>
       <h1>Create New F1 Team </h1>
-      <input type="text" placeholder='Team Name:' />
-      <input type="text" placeholder='Drivers:' />
-      <input type="text" placeholder='Base:' />
-      <input type="text" placeholder='Team Chief:' />
-      <input type="text" placeholder='Chassis:' />
-      <input type="text" placeholder='Logo(url):' />
-      <button>Create Team</button>
+
+      <form  onSubmit={handleSubmit}>
+      <input type="text" placeholder='Team Name:' onChange={e => setTeamName(e.target.value)} />
+      <input type="text" placeholder='Drivers:' onChange={e => setDrivers(e.target.value)} />
+      <input type="text" placeholder='Base:' onChange={e => setBase(e.target.value)} />
+      <input type="text" placeholder='Team Chief:' onChange={e => setTeamChief(e.target.value)} />
+      <input type="text" placeholder='Chassis:' onChange={e => setChassis(e.target.value)} />
+      <input type="text" placeholder='Logo(url):' onChange={e => setLogo(e.target.value)} />
+      </form>
+      <input type="submit" value="Create Team" />
 
     </div>
   )
